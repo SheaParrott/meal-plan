2e1d6704947241b302d70d26d1d50ee40a548cf0
SheaParrott <alexandergotthis@gmail.com>
2019-02-17 00:41:51 -0500

    home page basic layout created

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import front from './assets/front.jpg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Meal Plan</h1>
+        <input placeholder="search here!" />
+        <button>submit</button>
+        <img class="home" src={front} alt="cover" />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
