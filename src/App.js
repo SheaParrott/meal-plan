c209e556708e671797d62d86e8834246b7ae2696
SheaParrott <alexandergotthis@gmail.com>
2019-02-18 20:15:08 -0500

    Creator page added and styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import './App.css'
 import Home from './Home'
+import Creator from './Creator'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Home />
+        <Creator />
       </div>
     )
   }
