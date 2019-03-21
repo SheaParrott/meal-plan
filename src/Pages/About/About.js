ef17cc70ce345aa9a90f60cc03e04e41d8628159
SheaParrott <alexandergotthis@gmail.com>
2019-03-20 20:06:05 -0400

    about page fully responsive now, added in a special thanks to EDAMAM for providing the API.

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -5,9 +5,16 @@ import './style.css'
 import Footer from '../../Components/Footer'
 
 class About extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      InspirationShown: 'Inspiration'
+    }
+  }
   componentDidMount = () => {
     window.scrollTo(0, 0)
   }
+
   render() {
     return (
       <div className="spash-bg">
@@ -23,11 +30,77 @@ class About extends Component {
                     <div className="centerLine">
                       <div className="line" />
                     </div>
-                    <h5 className="about">Thanks for Visiting!</h5>
+                    <div className="thanksOrInspiration small-view">
+                      <h5
+                        className="about white"
+                        onClick={() => {
+                          this.setState({
+                            InspirationShown: 'Inspiration'
+                          })
+                        }}
+                      >
+                        INSPIRATION
+                      </h5>
+                      <h5
+                        className="about white"
+                        onClick={() => {
+                          this.setState({
+                            InspirationShown: 'thanks'
+                          })
+                        }}
+                      >
+                        SPECIAL THANKS
+                      </h5>
+                    </div>
+                    <h5 className="about big-view">Thanks for Visiting!</h5>
                   </nav>
                 </div>
-                <div className="aboutPageSpacing" />
-                <div className="aboutParagraph">
+                <div className="aboutPageSpacing " />
+                {this.state.InspirationShown == 'Inspiration' ? (
+                  <div className="aboutParagraph small-view">
+                    <h2 className="aboutInspiration">Inspiration</h2>
+                    <p className="about">
+                      <br /> Life is all about balance, right?
+                    </p>
+                    <p>
+                      <br />
+                      <strong>Well I think so!</strong>
+                    </p>
+                    <p className="about">
+                      <br /> A healthy diet is one of those balances most of us
+                      struggle with. This is something that I have realized and
+                      it has takes precedence in my day to day life. In an
+                      effort to better myself for a healthier lifestyle and help
+                      others <i>Meal Plan</i> was created! I hope this helps you
+                      as much as it will help me!
+                      <br />
+                      <br />
+                    </p>
+                    <p className="about"> -Blessings-</p>
+                  </div>
+                ) : (
+                  <div className="aboutParagraph small-view">
+                    <h2 className="aboutInspiration">Special Thanks!</h2>
+                    <p className="about">
+                      <br /> Thank you EDAMAM for providing the awesome and free
+                      API used in this project. Documentation can be found here:{' '}
+                      <strong>
+                        <a
+                          className="white"
+                          href="https://www.edamam.com/"
+                          target="_blank"
+                        >
+                          https://www.edamam.com/
+                        </a>
+                      </strong>{' '}
+                      <br />
+                      <br />
+                    </p>
+                    <p className="about"> -Blessings-</p>
+                  </div>
+                )}
+
+                <div className="aboutParagraph big-view">
                   <h2 className="aboutInspiration">Inspiration</h2>
                   <p className="about">
                     <br /> Life is all about balance, right?
@@ -48,26 +121,22 @@ class About extends Component {
                   </p>
                   <p className="about"> -Blessings-</p>
                 </div>
-                <div className="thanksParagraph">
-                  <h2 className="aboutInspiration">Inspiration</h2>
+                <div className="thanksParagraph big-view">
+                  <h2 className="aboutInspiration">Special Thanks!</h2>
                   <p className="about">
-                    <br /> Life is all about balance, right?
-                  </p>
-                  <p>
-                    <br />
-                    <strong>Well I think so!</strong>
-                  </p>
-                  <p className="about">
-                    <br /> A healthy diet is one of those balances most of us
-                    struggle with. This is something that I have realized and it
-                    has takes precedence in my day to day life. In an effort to
-                    better myself for a healthier lifestyle and help others{' '}
-                    <i>Meal Plan</i> was created! I hope this helps you as much
-                    as it will help me!
-                    <br />
+                    <br /> Thank you EDAMAM for providing the API used in this
+                    project. Documentation can be found here:{' '}
+                    <strong>
+                      <a
+                        className="white"
+                        href="https://www.edamam.com/"
+                        target="_blank"
+                      >
+                        https://www.edamam.com/
+                      </a>
+                    </strong>
                     <br />
                   </p>
-                  <p className="about"> -Blessings-</p>
                 </div>
                 <img className="home" src={front} alt="cover" />
               </div>
