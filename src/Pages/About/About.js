d6dcf16a8fd731f0437bb253ed5ee0ca7f149824
SheaParrott <alexandergotthis@gmail.com>
2019-03-21 21:24:38 -0400

    started changing the desktop view

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -18,94 +18,51 @@ class About extends Component {
   render() {
     return (
       <div className="spash-bg">
-        <div className="shaded-bg-lighter">
-          <div className="single-view-page-home ">
-            <div className="default">
-              <div className="default aboutPage">
-                <div>
-                  <nav>
-                    <h1 className="basicHeaderPlus">Meal Plan</h1>
-                    <Header />
+        {/* <div className="shaded-bg"> */}
+        <div className="single-view-page-home ">
+          <div className="default">
+            <div className="default aboutPage">
+              <div>
+                <nav>
+                  <h1 className="basicHeaderPlus">Meal Plan</h1>
+                  <Header />
 
-                    <div className="centerLine">
-                      <div className="line" />
-                    </div>
-                    <div className="thanksOrInspiration small-view">
-                      <h5
-                        className="about white"
-                        onClick={() => {
-                          this.setState({
-                            InspirationShown: 'Inspiration'
-                          })
-                        }}
-                      >
-                        INSPIRATION
-                      </h5>
-                      <h5
-                        className="about white"
-                        onClick={() => {
-                          this.setState({
-                            InspirationShown: 'thanks'
-                          })
-                        }}
-                      >
-                        SPECIAL THANKS
-                      </h5>
-                    </div>
-                    <h5 className="about big-view">Thanks for Visiting!</h5>
-                  </nav>
-                </div>
-                <div className="aboutPageSpacing " />
-                {this.state.InspirationShown == 'Inspiration' ? (
-                  <div className="aboutParagraph small-view">
-                    <h2 className="aboutInspiration">Inspiration</h2>
-                    <p className="about">
-                      <br /> Life is all about balance, right?
-                    </p>
-                    <p>
-                      <br />
-                      <strong>Well I think so!</strong>
-                    </p>
-                    <p className="about">
-                      <br /> A healthy diet is one of those balances most of us
-                      struggle with. This is something that I have realized and
-                      it has takes precedence in my day to day life. In an
-                      effort to better myself for a healthier lifestyle and help
-                      others <i>Meal Plan</i> was created! I hope this helps you
-                      as much as it will help me!
-                      <br />
-                      <br />
-                    </p>
-                    <p className="about"> -Blessings-</p>
+                  <div className="centerLine">
+                    <div className="line" />
                   </div>
-                ) : (
-                  <div className="aboutParagraph small-view">
-                    <h2 className="aboutInspiration">Special Thanks!</h2>
-                    <p className="about">
-                      <br /> Thank you EDAMAM for providing the awesome and free
-                      API used in this project. Documentation can be found here:{' '}
-                      <strong>
-                        <a
-                          className="white"
-                          href="https://www.edamam.com/"
-                          target="_blank"
-                        >
-                          https://www.edamam.com/
-                        </a>
-                      </strong>{' '}
-                      <br />
-                      <br />
-                    </p>
-                    <p className="about"> -Blessings-</p>
+                  <div className="thanksOrInspiration small-view">
+                    <h5
+                      className="about white"
+                      onClick={() => {
+                        this.setState({
+                          InspirationShown: 'Inspiration'
+                        })
+                      }}
+                    >
+                      INSPIRATION
+                    </h5>
+                    <h5
+                      className="about white"
+                      onClick={() => {
+                        this.setState({
+                          InspirationShown: 'thanks'
+                        })
+                      }}
+                    >
+                      SPECIAL THANKS
+                    </h5>
                   </div>
-                )}
-
-                <div className="aboutParagraph big-view">
+                  <h5 className="about big-view">Thanks for Visiting!</h5>
+                </nav>
+              </div>
+              <div className="aboutPageSpacing " />
+              {this.state.InspirationShown == 'Inspiration' ? (
+                <div className="aboutParagraph small-view">
                   <h2 className="aboutInspiration">Inspiration</h2>
-                  <p className="about">
+                  <p className="about aboutInspiration ">
                     <br /> Life is all about balance, right?
                   </p>
-                  <p>
+                  <p className="aboutInspiration">
                     <br />
                     <strong>Well I think so!</strong>
                   </p>
@@ -121,11 +78,12 @@ class About extends Component {
                   </p>
                   <p className="about"> -Blessings-</p>
                 </div>
-                <div className="thanksParagraph big-view">
+              ) : (
+                <div className="aboutParagraph small-view">
                   <h2 className="aboutInspiration">Special Thanks!</h2>
                   <p className="about">
-                    <br /> Thank you EDAMAM for providing the API used in this
-                    project. Documentation can be found here:{' '}
+                    <br /> Thank you EDAMAM for providing the awesome and free
+                    API used in this project. Documentation can be found here:{' '}
                     <strong>
                       <a
                         className="white"
@@ -134,16 +92,57 @@ class About extends Component {
                       >
                         https://www.edamam.com/
                       </a>
-                    </strong>
+                    </strong>{' '}
+                    <br />
                     <br />
                   </p>
+                  <p className="about aboutInspiration"> -Blessings-</p>
                 </div>
-                <img className="home" src={front} alt="cover" />
+              )}
+
+              <div className="aboutParagraph big-view">
+                <h2 className="aboutInspiration">Inspiration</h2>
+                <p className="about aboutInspiration">
+                  <br /> Life is all about balance, right?
+                </p>
+                <p className="aboutInspiration">
+                  <strong>Well I think so!</strong>
+                </p>
+                <p className="about">
+                  <br /> A healthy diet is one of those balances most of us
+                  struggle with. This is something that I have realized and it
+                  has takes precedence in my day to day life. In an effort to
+                  better myself for a healthier lifestyle and help others{' '}
+                  <i>Meal Plan</i> was created! I hope this helps you as much as
+                  it will help me!
+                  <br />
+                  <br />
+                </p>
+                <h2 className="aboutInspiration">Special Thanks!</h2>
+                <p className="about">
+                  <br /> Thank you EDAMAM for providing the API used in this
+                  project. Documentation can be found here:{' '}
+                  <strong>
+                    <a
+                      className=""
+                      href="https://www.edamam.com/"
+                      target="_blank"
+                    >
+                      https://www.edamam.com/
+                    </a>
+                  </strong>
+                  <br />
+                  <br />
+                </p>
+                <p className="about aboutInspiration"> -Blessings-</p>
               </div>
-              <Footer />
+
+              <img className="home small-view" src={front} alt="cover" />
             </div>
+            <Footer />
           </div>
         </div>
+        {/* </div> */}
       </div>
     )
   }
