6a41e678d02cfcf2344095e280b8c3100e13d53d
SheaParrott <alexandergotthis@gmail.com>
2019-03-23 23:22:37 -0400

    updated header and footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,15 +5,12 @@ class Footer extends Component {
   render() {
     return (
       <footer>
-        <span className="headerOrFooterBiggerScreens">
-          <Link to="/">
-            <p className="white footerButton">Home</p>
-          </Link>
-        </span>
+        <Link to="/">
+          <p className="white footerButton">Home</p>
+        </Link>
         <Link to="/creator">
           <p className="white footerButton">Creator</p>
         </Link>
-        {/* about page not created yet */}
         <Link to="/about">
           <p className="white footerButton">About</p>
         </Link>
