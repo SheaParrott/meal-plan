4bf552a34a97e2cfdef9d850578c3a52bd67acf6
SheaParrott <alexandergotthis@gmail.com>
2019-03-26 13:41:51 -0400

    minor changes in header and footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -8,11 +8,11 @@ class Footer extends Component {
         <Link to="/">
           <p className="white footerButton">Home</p>
         </Link>
-        <Link to="/creator">
-          <p className="white footerButton">Creator</p>
-        </Link>
         <Link to="/about">
           <p className="white footerButton">About</p>
+        </Link>
+        <Link to="/creator">
+          <p className="white footerButton">Creator</p>
         </Link>
         <a href="http://shea-portfolio.surge.sh/" target="_blank">
           <p className=" footerButton white">&copy;SheaParrott</p>
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,22 +4,15 @@ import { Link } from 'react-router-dom'
 class Header extends Component {
   render() {
     return (
-      // <div
-      //   className={
-      //     window.location.pathname == '/about'
-      //       ? 'categoriesBarAbout'
-      //       : 'categoriesBar'
-      //   }
-      // >
       <div className="categoriesBar">
         <Link to="/">
           <h4 className="homeButton white">HOME</h4>
         </Link>
-        <Link to="/creator">
-          <h4 className="homeButton white">CREATOR</h4>
-        </Link>
         <Link to="/about">
           <h4 className="homeButton white">ABOUT</h4>
+        </Link>
+        <Link to="/creator">
+          <h4 className="homeButton white">CREATOR</h4>
         </Link>
       </div>
     )
