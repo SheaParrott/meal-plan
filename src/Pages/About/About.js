acd06a1fc2b110a0cdf6c3fcd688626cee7314ae
SheaParrott <alexandergotthis@gmail.com>
2019-04-01 18:16:42 -0400

    added fade in and out annimation to about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -81,70 +81,84 @@ class About extends Component {
                   </nav>
                 </div>
                 <div className="aboutPageSpacing " />
-                {this.state.selected == 'Inspiration' ? (
-                  <div className="aboutParagraph small-view">
-                    <h2 className="aboutInspiration">Inspiration</h2>
-                    <p className="about aboutInspiration ">
-                      <br /> Life is all about balance, right?
-                    </p>
-                    <p className="aboutInspiration">
-                      <br />
-                      <strong>Well I think so!</strong>
-                    </p>
-                    <p className="about">
-                      <br /> A healthy diet is one of those balances most of us
-                      struggle with. This is something that I have realized and
-                      it has takes precedence in my day to day life. In an
-                      effort to better myself for a healthier lifestyle and help
-                      others <i>Meal Plan</i> was created! I hope this helps you
-                      as much as it will help me!
-                      <br />
-                      <br />
-                    </p>
-                    <p className="about"> -Blessings-</p>
-                  </div>
-                ) : this.state.selected == 'thanks' ? (
-                  <div className="aboutParagraph small-view">
-                    <h2 className="aboutInspiration">Special Thanks!</h2>
-                    <p className="about">
-                      <br /> Thank you EDAMAM for providing the API used in this
-                      project. Please go check out EDAMAM and give them your
-                      support! Documentation can be found here:{' '}
-                      <strong>
-                        <a
-                          className="EDAMAM-link white-mellow"
-                          href="https://www.edamam.com/"
-                          target="_blank"
-                        >
-                          https://www.edamam.com/
-                        </a>
-                      </strong>
-                      .
-                      <br />
-                      <br />
-                    </p>
-                    <p className="about aboutInspiration"> -Blessings-</p>
-                  </div>
-                ) : (
-                  <div className="aboutParagraph small-view">
-                    <h2 className="aboutInspiration">
-                      {' '}
-                      Enjoy The Site <br />
-                      <i>or</i> <br />
-                      Find An Issue?
-                    </h2>
-                    <p className="about">
-                      <br /> Check out the{' '}
-                      <Link className="EDAMAM-link" to="/creator">
-                        <strong>Creator</strong>{' '}
-                      </Link>
-                      page and reach out to me I would love to hear from you!
-                      <br />
-                      <br />
-                    </p>
-                    <p className="about aboutInspiration"> -Blessings-</p>
-                  </div>
-                )}
+                <div
+                  className={`aboutParagraph small-view ${
+                    this.state.selected == 'Inspiration'
+                      ? 'animated'
+                      : 'animated-hide'
+                  }`}
+                >
+                  <h2 className="aboutInspiration">Inspiration</h2>
+                  <p className="about aboutInspiration ">
+                    <br /> Life is all about balance, right?
+                  </p>
+                  <p className="aboutInspiration">
+                    <br />
+                    <strong>Well I think so!</strong>
+                  </p>
+                  <p className="about">
+                    <br /> A healthy diet is one of those balances most of us
+                    struggle with. This is something that I have realized and it
+                    has takes precedence in my day to day life. In an effort to
+                    better myself for a healthier lifestyle and help others{' '}
+                    <i>Meal Plan</i> was created! I hope this helps you as much
+                    as it will help me!
+                    <br />
+                    <br />
+                  </p>
+                  <p className="about"> -Blessings-</p>
+                </div>
+                <div
+                  className={`aboutParagraph small-view ${
+                    this.state.selected == 'thanks'
+                      ? 'animated'
+                      : 'animated-hide'
+                  }`}
+                >
+                  <h2 className="aboutInspiration">Special Thanks!</h2>
+                  <p className="about">
+                    <br /> Thank you EDAMAM for providing the API used in this
+                    project. Please go check out EDAMAM and give them your
+                    support! Documentation can be found here:{' '}
+                    <strong>
+                      <a
+                        className="EDAMAM-link white-mellow"
+                        href="https://www.edamam.com/"
+                        target="_blank"
+                      >
+                        https://www.edamam.com/
+                      </a>
+                    </strong>
+                    .
+                    <br />
+                    <br />
+                  </p>
+                  <p className="about aboutInspiration"> -Blessings-</p>
+                </div>
+                <div
+                  className={`aboutParagraph small-view ${
+                    this.state.selected == 'HappyOrIssues'
+                      ? 'animated'
+                      : 'animated-hide'
+                  }`}
+                >
+                  <h2 className="aboutInspiration">
+                    Enjoy The Site <br />
+                    <i>or</i> <br />
+                    Find An Issue?
+                  </h2>
+                  <p className="about">
+                    <br /> Check out the{' '}
+                    <Link className="EDAMAM-link" to="/creator">
+                      <strong>Creator</strong>{' '}
+                    </Link>
+                    page and reach out to me I would love to hear from you!
+                    <br />
+                    <br />
+                  </p>
+                  <p className="about aboutInspiration"> -Blessings-</p>
+                </div>
+                }
                 <div className="aboutSpacingFromNav" />
                 <section className="center-about">
                   <main className="big-view">
