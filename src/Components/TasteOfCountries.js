e497525fcd835a012bc40821c4a7488f1f4abfed
SheaParrott <alexandergotthis@gmail.com>
2019-04-02 10:26:18 -0400

    Fixed countries see more bug

--- src/Components/TasteOfCountries.js
+++ src/Components/TasteOfCountries.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react'
 import HomeRecipe from './HomeRecipe'
-import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
+import { SearchedRecipe } from '../Actions/SearchActions'
+import { Redirect } from 'react-router'
 
 class TasteOfCountries extends Component {
+  constructor(props) {
+    super(props)
+    this._SearchedRecipe = this._SearchedRecipe.bind(this)
+    this.state = {
+      navigate: false
+    }
+  }
+  _SearchedRecipe = () => {
+    this.props._SearchedRecipe(this.props.country.country)
+    this.setState({
+      navigate: true
+    })
+  }
   render() {
+    if (this.state.navigate) {
+      return (
+        <Redirect
+          to={`/results/&q=${this.props.country.country}${
+            this.props.from.param
+          }${this.props.toParam.param}`}
+          push={true}
+        />
+      )
+    }
     return (
       <div className="country-container">
         <h3 className="country-slider">{this.props.country.country}</h3>
@@ -13,13 +37,9 @@ class TasteOfCountries extends Component {
             return <HomeRecipe key={recipe.uri} recipe={recipe} taste={true} />
           })}
         </div>
-        <Link
-          to={`/results/&q=${this.props.country.country}${
-            this.props.from.param
-          }${this.props.toParam.param}`}
-        >
-          <h4 className="seeMore green">See More</h4>
-        </Link>
+        <h4 onClick={this._SearchedRecipe} className="seeMore green">
+          See More
+        </h4>
       </div>
     )
   }
@@ -30,7 +50,9 @@ const mapStateToProps = state => ({
   toParam: state.toParam
 })
 
-const mapActionsToProps = {}
+const mapActionsToProps = {
+  _SearchedRecipe: SearchedRecipe
+}
 
 export default connect(
   mapStateToProps,
